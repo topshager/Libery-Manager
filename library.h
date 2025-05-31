9fe1be6d59077f1281a1c638bdacfb42fa234cc7
topshager <rem28777@gmail.com>
2025-05-31 23:44:27 +0200

    Update library.h

--- library.h
+++ library.h
@@ -2,9 +2,14 @@
 using namespace std;
 class LibreryItem
 {
+private:
     std::string title;
     std::string author;
     int year;
     std::string id;
+public:
+    virtual void displayInfo() const;
+    virtual ~LibreryItem();
+
 };
  
