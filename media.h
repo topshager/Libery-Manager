f1243aabed3d445cfa4449e30a5bdf60496332be
topshager <rem28777@gmail.com>
2025-07-02 00:12:22 +0200

    Update media.h

--- media.h
+++ media.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring> 
 using  namespace std;
 
 
@@ -10,21 +11,24 @@ private:
 
 public:
 
-    Media(char *t, int rating){
+    Media(const char *t, int r){
         title = new char[strlen(t)+1];
         strcpy(title,t);
+        rating = r;
     }
     
     Media(const Media& b){
         title = new char[strlen(b.title)+1];
         strcpy(title,b.title);
+        rating = b.rating;
     }
-    
+
     Media& operator=(const Media& b){
         if (this == &b) return *this;
         delete[] title;
         title = new char[strlen(b.title)+1];
         strcpy(title,b.title);
+        rating = b.rating;
         return *this;
     }
     ~Media(){
