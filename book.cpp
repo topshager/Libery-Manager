0d80234a005578df0799e4eb3a81e54095a9f5b7
topshager <rem28777@gmail.com>
2025-07-02 19:41:37 +0200

    defintion of book.cpp

--- book.cpp
+++ book.cpp
@@ -1,1 +1,37 @@
-#include "book.h"
+#include "book.h";
+#include <iostream>
+#include <cstring>
+
+    Book::Book(const char*  t,int r,const char* a ): Media(t,r)
+    {
+        author = new char[strlen(a)+1];
+        strcpy(author,a);
+    }
+
+    Book::Book(const Book& b): Media(b)
+    {
+        author = new char[strlen(b.author)+1];
+        strcpy(author,b.author);
+    }
+
+    Book& Book::operator=(const Book& b){
+        if (this == &b)
+            return *this;
+
+        Media::operator=(b);
+        delete[]   author ;
+
+        author= new char[strlen(b.author)+1];
+        strcpy(author,b.author);
+
+        return *this;
+    }
+
+    Book::~Book(){
+        delete [] author;
+    }
+
+    void Book::show() {
+        Media::show();  
+        std::cout << "Author: " << author << std::endl;
+    }
