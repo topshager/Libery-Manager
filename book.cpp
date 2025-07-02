ead3fe2630506f2b0aae4a1e438f16ecf4c9dee1
topshager <rem28777@gmail.com>
2025-07-02 23:30:24 +0200

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -8,7 +8,7 @@
         ISB = new char[strlen(i)+1];
         Copies = new int(*c) ;
         category = new char[strlen(ctg)+1];
-        
+
         strcpy(author,a);
         strcpy(ISB,i);
         strcpy(category,ctg);
@@ -17,7 +17,11 @@
     Book::Book(const Book& b): Media(b)
     {
         author = new char[strlen(b.author)+1];
+        ISB = new char[strlen(b.ISB)+1];
+        category =  new char[strlen(b.category)+1];
         strcpy(author,b.author);
+        strcpy(ISB,b.ISB);
+        strcpy(category ,b.category);
     }
 
     Book& Book::operator=(const Book& b){
@@ -26,9 +30,15 @@
 
         Media::operator=(b);
         delete[]   author ;
+        delete[]   ISB ;
+        delete[]   Copies;
+        delete[]   category ;
+        
 
         author= new char[strlen(b.author)+1];
         strcpy(author,b.author);
+        strcpy(ISB,b.ISB);
+        strcpy(category ,b.category);
 
         return *this;
     }
