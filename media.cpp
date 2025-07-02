ffb73102f113265d7be6585ec671af41a4a9130b
topshager <rem28777@gmail.com>
2025-07-02 19:41:52 +0200

    Defintion of media.cpp

--- media.cpp
+++ media.cpp
@@ -1,1 +1,29 @@
 #include "media.h";
+
+    Media::Media(const char *t, int r){
+        title = new char[strlen(t)+1];
+        strcpy(title,t);
+        rating = r;
+    }
+    
+    Media::Media(const Media& b){
+        title = new char[strlen(b.title)+1];
+        strcpy(title,b.title);
+        rating = b.rating;
+    }
+
+    Media& Media:: operator=(const Media& b){
+        if (this == &b) return *this;
+        delete[] title;
+        title = new char[strlen(b.title)+1];
+        strcpy(title,b.title);
+        rating = b.rating;
+        return *this;
+    }
+   Media::~Media(){
+        delete [] title;
+    }
+
+    void Media::show() {
+        Media::show();
+    }
