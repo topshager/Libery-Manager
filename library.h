1708ef613deca64bcd5bb2c5c2a80566c12ed8d0
topshager <rem28777@gmail.com>
2025-07-04 17:34:27 +0200

    Update library.h

--- library.h
+++ library.h
@@ -2,9 +2,10 @@
 #define LIBARY_H_
 #include "book.h"
 
-int addBook(const char*  t,int r,const char* a,char *i,int* c,char* ctg );
-int subtractBook(int a, int b);
-int multiply(int a , int b);
-double divide(int a, int b);
-
+class Libary : public Book
+{
+public:    
+    void addBook();
+    int subtractBook(int a, int b);
+};
 #endif
